3858323528e633e567333930d58acd9165fa5463
nutcas3 <mauricee423@gmail.com>
2023-10-04 15:37:20 +0300

    handle client function done

--- src/main.rs
+++ src/main.rs
@@ -22,3 +22,28 @@ const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n
 const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 const INTERNAL_ERROR: &str = "HTTP/1.1 500 INTERNAL ERROR\r\n\r\n";
 
+fn handle_client(mut stream:TcpStream) {
+    let mut buffer = [0; 1024];
+    let mut request = String::new();
+
+
+    match stream.read(&mut buffer){
+        Ok(size)=>{
+            request.push_str(String::from_utf8_lossy(&buffer[..size]).as_ref());
+
+
+            let (status_line,content) = match &*request{
+                r if r.starts_with("POST /users")=> handle_post_request(r),
+                r if r.starts_with("GET /users/")=> handle_get_request(r),
+                r if r.starts_with("GET /users/")=> handle_get_all_request(r),
+                r if r.starts_with("PUT /users/")=> handle_put_request(r),
+                r if r.starts_with("DELETE /users/")=> handle_delete_request(r),
+                _ => (NOT_FOUND.to_string()," 404 not found".to_string()),
+            };
+
+            stream.write_all(format!("{}{}",status_line,content).as_bytes()).unwrap();
+        }
+        Err(e) => eprintln!("Unable to read stream: {}",e)
+    }
+    
+}
